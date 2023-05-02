c2b4fda5efbf52e7cda2cc96f99bd2dc85a190c0
lucarebuffi <lrebuffi@anl.gov>
2023-05-02 16:39:17 -0500

    Added Java-like reflection library

--- aps/common/reflection.py
+++ aps/common/reflection.py
@@ -1,7 +1,9 @@
-# #########################################################################
-# Copyright (c) 2022, UChicago Argonne, LLC. All rights reserved.         #
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------- #
+# Copyright (c) 2023, UChicago Argonne, LLC. All rights reserved.         #
 #                                                                         #
-# Copyright 2022. UChicago Argonne, LLC. This software was produced       #
+# Copyright 2023. UChicago Argonne, LLC. This software was produced       #
 # under U.S. Government contract DE-AC02-06CH11357 for Argonne National   #
 # Laboratory (ANL), which is operated by UChicago Argonne, LLC for the    #
 # U.S. Department of Energy. The U.S. Government has rights to use,       #
@@ -41,97 +43,48 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN       #
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         #
 # POSSIBILITY OF SUCH DAMAGE.                                             #
-# #########################################################################
+# ----------------------------------------------------------------------- #
+import importlib
 
-import os
+def class_for_name(module_name, class_name):
+    try:
+        module = importlib.import_module(module_name)
+        try:  __class = getattr(module, class_name)
+        except AttributeError: raise ValueError('Class ' + module_name + "." + class_name + ' does not exist')
+    except ImportError: raise ValueError('Module ' + module_name + ' does not exist')
 
-try:
-    from setuptools import find_packages, setup
-except AttributeError:
-    from setuptools import find_packages, setup
+    return __class
 
-NAME = 'aps_common_libraries'
+def instance_for_name(module_name, class_name, **arguments):
+    return class_for_name(module_name, class_name)(**arguments)
 
-VERSION = '1.0.3'
-ISRELEASED = False
+def method_of_module_for_name(module_name, method_name, **arguments):
+    try: module = importlib.import_module(module_name)
+    except ImportError: raise ValueError('Module ' + module_name + ' does not exist')
 
-DESCRIPTION = 'APS Common Libraries'
-README_FILE = os.path.join(os.path.dirname(__file__), 'README.md')
-LONG_DESCRIPTION = open(README_FILE).read()
-AUTHOR = 'Luca Rebuffi'
-AUTHOR_EMAIL = 'lrebuffi@anl.gov'
-URL = 'https://github.com/APS-XSD-OPT-Group/Common-Libraries'
-DOWNLOAD_URL = 'https://github.com/APS-XSD-OPT-Group/Common-Libraries'
-MAINTAINER = 'XSD-OPT Group @ APS-ANL'
-MAINTAINER_EMAIL = 'lrebuffi@anl.gov'
-LICENSE = 'BSD-3'
+    try: return getattr(module, method_name)(**arguments)
+    except AttributeError: raise ValueError('Method ' + method_name + ' does not exist')
 
-KEYWORDS = ['dictionary',
-    'glossary',
-    'synchrotron'
-    'simulation',
-]
+def method_of_class_for_name(module_name, class_name, method_name, **arguments):
+    __class = class_for_name(module_name, class_name)
 
-CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
-    'License :: OSI Approved :: BSD License',
-    'Natural Language :: English',
-    'Environment :: Console',
-    'Environment :: Plugins',
-    'Programming Language :: Python :: 3.8',
-    'Topic :: Scientific/Engineering :: Visualization',
-    'Intended Audience :: Science/Research',
-]
+    try: return getattr(__class, method_name)(**arguments)
+    except AttributeError: raise ValueError('Method ' + method_name + ' does not exist')
 
-INSTALL_REQUIRES = (
-    'setuptools',
-    'numpy<1.23,>=1.21',
-    'scipy<=1.9.1,>=1.7.3',
-    'matplotlib<=3.5.3,>=3.3.2',
-    'PyQt5==5.15.2',
-    'scikit-image',
-    'opencv-python',
-    'h5py',
-    'numba',
-    'torch<=1.13.0',
-    'pyepics',
-)
+def call_method_of_instance(instance, method_name, **arguments):
+    try: return getattr(instance, method_name)(**arguments)
+    except AttributeError: raise ValueError('Method ' + method_name + ' does not exist')
 
-SETUP_REQUIRES = (
-    'setuptools',
-)
+def get_class_name_and_module(full_name : str):
+    try:
+        tokens      = full_name.split(sep=".")
+        class_name  = tokens[-1]
+        module_name = full_name[:-(len(class_name)+1)]
 
-PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
+        return class_name, module_name
+    except:
+        raise ValueError("Malformed Class Name: " + full_name)
 
-PACKAGE_DATA = {
-}
-
-NAMESPACE_PACAKGES = ["aps", "aps.common"]
-
-def setup_package():
-
-    setup(
-        name=NAME,
-        version=VERSION,
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        author=AUTHOR,
-        author_email=AUTHOR_EMAIL,
-        maintainer=MAINTAINER,
-        maintainer_email=MAINTAINER_EMAIL,
-        url=URL,
-        download_url=DOWNLOAD_URL,
-        license=LICENSE,
-        keywords=KEYWORDS,
-        classifiers=CLASSIFIERS,
-        packages=PACKAGES,
-        package_data=PACKAGE_DATA,
-        namespace_packages=NAMESPACE_PACAKGES,
-        zip_safe=False,
-        include_package_data=True,
-        install_requires=INSTALL_REQUIRES,
-        setup_requires=SETUP_REQUIRES,
-    )
-
-if __name__ == '__main__':
-    setup_package()
+if __name__=="__main__":
+    print(get_class_name_and_module("aps.common.ciccio.Pelliccio"))
+    print(get_class_name_and_module("CiccioPelliccio"))
