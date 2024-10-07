105566840f2b896aa93db97754f780233ced70e9
lrebuffi <lrebuffi@anl.gov>
2024-10-07 12:44:58 -0500

    3-ID Beam Manager

--- aps/common/measurment/beamline/image_collector.py
+++ aps/common/measurment/beamline/image_collector.py
@@ -63,8 +63,29 @@ ini_file = get_registered_ini_instance(APPLICATION_NAME)
 WAIT_TIME         = ini_file.get_float_from_ini(section="Execution", key="Wait-Time",     default=0.1)
 EXPOSURE_TIME     = ini_file.get_float_from_ini(section="Execution", key="Exposure-Time", default=0.3)
 
+CAM_ACQUIRE        = ini_file.get_string_from_ini(section="Epics", key="Cam-Acquire",         default="dp_andor3_skylark:cam1:Acquire")
+CAM_EXPOSURE_TIME  = ini_file.get_string_from_ini(section="Epics", key="Cam-Exposure-Time",   default="dp_andor3_skylark:cam1:AcquireTime")
+CAM_IMAGE_MODE     = ini_file.get_string_from_ini(section="Epics", key="Cam-Image-Mode",      default="dp_andor3_skylark:cam1:ImageMode")
+TIFF_FILENAME      = ini_file.get_string_from_ini(section="Epics", key="Tiff-File-Name",      default="dp_andor3_skylark:TIFF1:FileName")
+TIFF_FILEPATH      = ini_file.get_string_from_ini(section="Epics", key="Tiff-File-Path",      default="dp_andor3_skylark:TIFF1:FilePath")
+TIFF_FILENUMBER    = ini_file.get_string_from_ini(section="Epics", key="Tiff-File-Number",    default="dp_andor3_skylark:TIFF1:FileNumber")
+TIFF_AUTOSAVE      = ini_file.get_string_from_ini(section="Epics", key="Tiff-Auto-Save",      default="dp_andor3_skylark:TIFF1:AutoSave")
+TIFF_SAVEFILE      = ini_file.get_string_from_ini(section="Epics", key="Tiff-Write-File",     default="dp_andor3_skylark:TIFF1:WriteFile")
+TIFF_AUTOINCREMENT = ini_file.get_string_from_ini(section="Epics", key="Tiff-Auto-Increment", default="dp_andor3_skylark:TIFF1:AutoIncrement")
+
 ini_file.set_value_at_ini(section="Execution",   key="Wait-Time",     value=WAIT_TIME)
 ini_file.set_value_at_ini(section="Execution",   key="Exposure-Time", value=EXPOSURE_TIME)
+
+ini_file.set_value_at_ini(section="Epics", key="Cam-Acquire",         value=CAM_ACQUIRE       )
+ini_file.set_value_at_ini(section="Epics", key="Cam-Exposure-Time",   value=CAM_EXPOSURE_TIME )
+ini_file.set_value_at_ini(section="Epics", key="Cam-Image-Mode",      value=CAM_IMAGE_MODE    )
+ini_file.set_value_at_ini(section="Epics", key="Tiff-File-Name",      value=TIFF_FILENAME     )
+ini_file.set_value_at_ini(section="Epics", key="Tiff-File-Path",      value=TIFF_FILEPATH     )
+ini_file.set_value_at_ini(section="Epics", key="Tiff-File-Number",    value=TIFF_FILENUMBER   )
+ini_file.set_value_at_ini(section="Epics", key="Tiff-Auto-Save",      value=TIFF_AUTOSAVE     )
+ini_file.set_value_at_ini(section="Epics", key="Tiff-Write-File",     value=TIFF_SAVEFILE     )
+ini_file.set_value_at_ini(section="Epics", key="Tiff-Auto-Increment", value=TIFF_AUTOINCREMENT)
+
 ini_file.push()
 
 IMAGE_COLLECTOR_STATUS_FILE = "image_collector_status.pkl"
@@ -80,17 +101,17 @@ class ImageCollector():
         self.__mocking_mode          = mocking_mode
 
         if not self.__mocking_mode:
-            self.__PV_dict = dict(
-                andor_cam_acquire        = PV("dp_andor3_skylark:cam1:Acquire"),  # 0="Done", 1="Acquire"
-                andor_cam_exposure_time  = PV("dp_andor3_skylark:cam1:AcquireTime"),
-                andor_cam_image_mode     = PV("dp_andor3_skylark:cam1:ImageMode"),    # "Fixed" or "Continuous"
-                andor_tiff_filename      = PV("dp_andor3_skylark:TIFF1:FileName"),
-                andor_tiff_filepath      = PV("dp_andor3_skylark:TIFF1:FilePath"),
-                andor_tiff_filenumber    = PV("dp_andor3_skylark:TIFF1:FileNumber"),
-                andor_tiff_autosave      = PV("dp_andor3_skylark:TIFF1:AutoSave"),
-                andor_tiff_savefile      = PV("dp_andor3_skylark:TIFF1:WriteFile"),
-                andor_tiff_autoincrement = PV("dp_andor3_skylark:TIFF1:AutoIncrement")
-            )
+            self.__PV_dict = {
+                "cam_acquire"        : PV(CAM_ACQUIRE),       # 0="Done", 1="Acquire"
+                "cam_exposure_time"  : PV(CAM_EXPOSURE_TIME),
+                "cam_image_mode"     : PV(CAM_IMAGE_MODE),    # "Fixed" or "Continuous"
+                "tiff_filename"      : PV(TIFF_FILENAME),
+                "tiff_filepath"      : PV(TIFF_FILEPATH),
+                "tiff_filenumber"    : PV(TIFF_FILENUMBER),
+                "tiff_autosave"      : PV(TIFF_AUTOSAVE),
+                "tiff_savefile"      : PV(TIFF_SAVEFILE),
+                "tiff_autoincrement" : PV(TIFF_AUTOINCREMENT)
+            }
 
             if detector_delay is None:
                 self.__has_delay = False
@@ -99,7 +120,7 @@ class ImageCollector():
                 self.__detector_delay = detector_delay
 
             self.__detector_stop()
-            self.__set_andor_defaults(1)
+            self.__set_defaults(1)
         else:
             print("ImageCollector initialized in Mocking Mode")
 
@@ -108,12 +129,12 @@ class ImageCollector():
             self.__detector_stop()
 
             dictionary = OrderedDict()
-            dictionary["andor_cam_image_mode"]     = self.__PV_dict["andor_cam_image_mode"].get()
-            dictionary["andor_cam_exposure_time"]  = self.__PV_dict["andor_cam_exposure_time"].get()
-            dictionary["andor_tiff_filepath"]      = self.__PV_dict["andor_tiff_filepath"].get()
-            dictionary["andor_tiff_filenumber"]    = self.__PV_dict["andor_tiff_filenumber"].get()
-            dictionary["andor_tiff_savefile"]      = self.__PV_dict["andor_tiff_savefile"].get()
-            dictionary["andor_tiff_autoincrement"] = self.__PV_dict["andor_tiff_autoincrement"].get()
+            dictionary["cam_image_mode"]     = self.__PV_dict["cam_image_mode"].get()
+            dictionary["cam_exposure_time"]  = self.__PV_dict["cam_exposure_time"].get()
+            dictionary["tiff_filepath"]      = self.__PV_dict["tiff_filepath"].get()
+            dictionary["tiff_filenumber"]    = self.__PV_dict["tiff_filenumber"].get()
+            dictionary["tiff_savefile"]      = self.__PV_dict["tiff_savefile"].get()
+            dictionary["tiff_autoincrement"] = self.__PV_dict["tiff_autoincrement"].get()
 
             file = open(IMAGE_COLLECTOR_STATUS_FILE, 'wb')
             pickle.dump(dictionary, file)
@@ -127,12 +148,12 @@ class ImageCollector():
             dictionary = pickle.load(file)
             file.close()
 
-            self.__PV_dict["andor_cam_image_mode"].put(    dictionary["andor_cam_image_mode"])
-            self.__PV_dict["andor_cam_exposure_time"].put( dictionary["andor_cam_exposure_time"])
-            self.__PV_dict["andor_tiff_filepath"].put(     dictionary["andor_tiff_filepath"])
-            self.__PV_dict["andor_tiff_filenumber"].put(   dictionary["andor_tiff_filenumber"])
-            self.__PV_dict["andor_tiff_savefile"].put(     dictionary["andor_tiff_savefile"])
-            self.__PV_dict["andor_tiff_autoincrement"].put(dictionary["andor_tiff_autoincrement"])
+            self.__PV_dict["cam_image_mode"].put(    dictionary["cam_image_mode"])
+            self.__PV_dict["cam_exposure_time"].put( dictionary["cam_exposure_time"])
+            self.__PV_dict["tiff_filepath"].put(     dictionary["tiff_filepath"])
+            self.__PV_dict["tiff_filenumber"].put(   dictionary["tiff_filenumber"])
+            self.__PV_dict["tiff_savefile"].put(     dictionary["tiff_savefile"])
+            self.__PV_dict["tiff_autoincrement"].put(dictionary["tiff_autoincrement"])
 
     def save_status(self):
         self.__to_pickle_file()
@@ -151,8 +172,8 @@ class ImageCollector():
 
     def end_collection(self): # to be done at the end of the data collection
         if not self.__mocking_mode:
-            self.__PV_dict["andor_tiff_autosave"].put("No")
-            self.__PV_dict["andor_tiff_autoincrement"].put("Yes")
+            self.__PV_dict["tiff_autosave"].put("No")
+            self.__PV_dict["tiff_autoincrement"].put("Yes")
 
     def get_total_acquisition_time(self):
         return 3*WAIT_TIME + self.__exposure_time
@@ -162,31 +183,31 @@ class ImageCollector():
         self.__detector_delay()
 
     def __detector_start(self):
-        self.__PV_dict["andor_cam_acquire"].put(1)
+        self.__PV_dict["cam_acquire"].put(1)
         time.sleep(WAIT_TIME)
 
     def __detector_stop(self):
-        self.__PV_dict["andor_cam_acquire"].put(0)
+        self.__PV_dict["cam_acquire"].put(0)
         time.sleep(WAIT_TIME)
 
     def __detector_acquiring(self):
-        return self.__PV_dict["andor_cam_acquire"].get() in (1, "Acquiring")
+        return self.__PV_dict["cam_acquire"].get() in (1, "Acquiring")
 
     def __detector_done(self):
-        return self.__PV_dict["andor_cam_acquire"].get() in (0, "Done")
+        return self.__PV_dict["cam_acquire"].get() in (0, "Done")
 
     def __initialize_current_image(self, index):
         self.__detector_stop()  # 1 waiting time
-        self.__set_andor_defaults(index)
+        self.__set_defaults(index)
 
-    def __set_andor_defaults(self, index):
-        self.__PV_dict["andor_cam_image_mode"].put("Fixed")
-        self.__PV_dict["andor_cam_exposure_time"].put(self.__exposure_time)
-        self.__PV_dict["andor_tiff_filepath"].put(self.__measurement_directory)
-        self.__PV_dict["andor_tiff_autosave"].put("Yes")
-        self.__PV_dict["andor_tiff_autoincrement"].put("No")
-        self.__PV_dict["andor_tiff_filename"].put(self.__file_name_prefix)
-        if index > 0: self.__PV_dict["andor_tiff_filenumber"].put(index)
+    def __set_defaults(self, index):
+        self.__PV_dict["cam_image_mode"].put("Fixed")
+        self.__PV_dict["cam_exposure_time"].put(self.__exposure_time)
+        self.__PV_dict["tiff_filepath"].put(self.__measurement_directory)
+        self.__PV_dict["tiff_autosave"].put("Yes")
+        self.__PV_dict["tiff_autoincrement"].put("No")
+        self.__PV_dict["tiff_filename"].put(self.__file_name_prefix)
+        if index > 0: self.__PV_dict["tiff_filenumber"].put(index)
 
     def __detector_delay(self):
         if not self.__has_delay:
